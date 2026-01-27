cc8a2c786271b51c26f8e66b3b3c675335d38777
Rithish Kannan <rithish4610@gmail.com>
2026-01-27 14:27:02 +0530

    Movies listing coming soon!

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,8 @@ from flask import render_template
 def home():
     return render_template('home.html')
 
+
+@app.route('/movies')
+def movies():
+    return '<h2>Movies listing coming soon!</h2>'
 # More routes will be added here
